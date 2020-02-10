3ac67a75c617c8d88daa4a952c803ad18fa662ba
himself65 <himself65@outlook.com>
2020-02-10 17:59:18 +0800

    init repo

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+require('dotenv').config()
+import 'puppeteer'
+import * as Agent from 'socks5-https-client/lib/Agent'
+import * as TelegramBot from 'node-telegram-bot-api'
+
+const TOKEN = process.env.BOT_TOKEN
+
+let proxy
+if (process.env.SOCKS_HOST && process.env.SOCKS_PORT) {
+  proxy = {
+    agentClass: Agent,
+    agentOptions: {
+      socksPort: process.env.SOCKS_PORT,
+      socksHost: process.env.SOCKS_HOST
+    }
+  }
+}
+
+const bot = new TelegramBot(TOKEN, {
+  polling: true,
+  request: proxy
+})
+
+bot.onText(/\/help/, async (msg, match) => {
+  await bot.sendMessage(msg.chat.id, `
+/help: display all commands
+`)
+})
